204440d63e30b30579015c217d435e7ab8cd1b4b
Pengfei Xu <pengfei.xu@intel.com>
2023-10-06 15:54:49 +0800

    main.rs: imrpove the print
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,11 +1,11 @@
 fn greet_world() {
-    let southern_germany: &str = "Grüß Gott!";
-    let chinese = "世界，你好";
-    let english = "World, hello";
+    let southern_germany: &str = "Grüß Gott!\n";
+    let chinese = "世界，你好.\n";
+    let english = "World, hello.\n";
     let hi_rust: &str = "Hello rust!\n";
     let regions: [&str; 4] = [southern_germany, chinese, english, hi_rust];
     for region in regions.iter() {
-        println!("{}", &region);
+        print!("{}", &region);
     }
 }
 
