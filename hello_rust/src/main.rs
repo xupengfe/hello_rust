fc1257996b01ba22d478307e34f46666571cbcab
Pengfei Xu <pengfei.xu@intel.com>
2023-10-06 15:52:42 +0800

    main.rs: improve the hello print
    Signed-off-by: Pengfei Xu <pengfei.xu@intel.com>

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,14 @@
+fn greet_world() {
+    let southern_germany: &str = "Grüß Gott!";
+    let chinese = "世界，你好";
+    let english = "World, hello";
+    let hi_rust: &str = "Hello rust!\n";
+    let regions: [&str; 4] = [southern_germany, chinese, english, hi_rust];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    greet_world();
 }
